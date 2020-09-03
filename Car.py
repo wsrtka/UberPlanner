bbd20b6903eb20ab0abf1c03c4535ab79bd5e958
wsrtka <witold.serwatka@pm.me>
2020-09-03 11:30:47 +0200

    changed class constructors and used setters instead

--- Car.py
+++ Car.py
@@ -2,14 +2,32 @@
 
 class Car:
 
-    def __init__(self, manufacturer, model, color, plate, body=None, fuel=None, card=None, pin=None):
+    def __init__(self):
+        self.rating = 3
 
+    def set_manufacturer(self, manufacturer):
         self.manufacturer = manufacturer
+
+    def set_model(self, model):
         self.model = model
+
+    def set_color(self, color):
         self.color = color
+
+    def set_plate(self, plate):
         self.plate = plate
-        
+
+    def set_body(self, body):
         self.body = body
+
+    def set_fuel(self, fuel):
         self.fuel = fuel
+
+    def set_card(self, card):
         self.card = card
+
+    def set_pin(self, pin):
         self.pin = pin
+
+    def set_rating(self, rating):
+        self.rating = rating
--- Driver.py
+++ Driver.py
@@ -2,8 +2,14 @@
 
 class Driver:
 
-    def __init__(self, name, surname):
-
-        self.name = name
-        self.surname = surname
+    def __init__(self):
         self.priority = False
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_surname(self, surname):
+        self.surname = surname
+
+    def set_priority(self, priority):
+        self.priority = priority
